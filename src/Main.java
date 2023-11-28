3a5c0999b94ed7a68617e5bfbd6ddd1abeb83968
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-28 16:19:34 -0300

    aula 1

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    // método = função
+    // o método principal é resposável por dar inicio a execução de um programa em java
+    public static void main(String[] args) {
+
+        // Tipagem Dinâmica - JavaScript (Interpretado) (Client-Side)
+        // Tipagem Estática - Java (Compilado) (Server-Side)
+
+        // int -> Integer -> Inteiro (Numerico Inteiro)
+        // long -> Long -> Longo (Numero Inteiro)
+        // double -> Double -> Dobro (Numerico Decimal)
+        // float -> Float -> Flutuante (Numerico Decimal)
+        // boolean -> Boolean (Lógico)
+        // char -> Charactere -> Caractere (Numerico/Textual)
+        // String -> Sequência ou Cadeia (Textuais)
+
+        int integer = 2147483647;
+        long inteiroLongo = 214748366456448L;
+        double flutuanteLongo = 474836645644.5466;
+        float flutuante = 5435345.67F;
+        boolean logico = false;
+        char letra = 'I';
+        letra = 'G';
+        integer = 17;
+        // integer = "Oi";
+        String nome = "Gabriel gosta de chocolate.";
+
+        System.out.println(nome);
+        System.out.println(flutuante);
+    }
+}
