4a6d37e9034a183f5b32f10262a0887b09c11eae
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-12-01 10:43:09 -0300

    metodos

---
+++ src/aula_07/Computador.java
@@ -0,0 +1,13 @@
+package aula_07;
+
+public class Computador {
+  
+  void ligar() {
+    System.out.println("Ligando..");
+    System.out.println("Iniciado.");
+  }
+
+  void executar(String programa) {
+    System.out.println("Executando " + programa + "...");
+  }
+}
---
+++ src/aula_07/Metodos.java
@@ -0,0 +1,40 @@
+package aula_07;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+// modificador de acesso -> public, private
+// define que o método é membro da classe -> static
+public class Metodos {
+
+  static double raizQuadrada(double valor) {
+    return Math.sqrt(valor);
+  }
+
+  static void bemVindo(String nome) {
+    System.out.println("Bem vindo(a), " + nome);
+  }
+
+  static void mostrarDataHora() {
+    LocalDateTime dataHora = LocalDateTime.now();
+    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    System.out.println(formatador.format(dataHora));
+  }
+
+  public static void main(String[] args) {
+    double raiz = raizQuadrada(400);
+    System.out.println(raiz);
+
+    bemVindo("Gabriel Braga");
+    mostrarDataHora();
+
+    Computador comp = new Computador();
+    comp.ligar();
+    comp.executar("Visual Studio Code");
+  }
+}
+
+// Metodos metodos = new Metodos();
+// metodos.main(); // membro do objeto (sem static)
+// Metodos.main(); // membro da classe (com static)
